59f0edf7d6677e8b7db7ff9bec870f1b41687f98
amitkumarashutosh <amitkumarashutos@gmail.com>
2024-12-10 19:41:17 +0530

    client side, react router initial setup

---
+++ client/src/pages/CaptainSignup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CaptainSignup = () => {
+  return (
+    <div>
+      <p>captain signup</p>
+    </div>
+  );
+};
+
+export default CaptainSignup;
---
+++ client/src/pages/UserSignup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserSignup = () => {
+  return (
+    <div>
+      <p>user signup</p>
+    </div>
+  );
+};
+
+export default UserSignup;
